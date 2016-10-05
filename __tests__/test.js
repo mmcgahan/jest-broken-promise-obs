08bf9253b84727b67349e3c86448b4911b96ccd6
Michael McGahan <mike.mcgahan@gmail.com>
2016-10-06 11:50:38 +1300

    remove rxjs dependency, clarify examples

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,55 +1,27 @@
-const { Observable } = require('rxjs');
-
 describe('from "of"', () => {
-	it('succeeds for passing expect', (done) => {
 
-		Observable.of(1)
-			.do(() => expect(true).toBe(true))  // run the test
-			.subscribe(null, null, done);  // on complete, call done
-
-	});
-	it('fails normally for failing expect', (done) => {
-
-		Observable.of(1)
-			.do(() => expect(true).toBe(false))  // fails normally
-			.subscribe(null, null, done);
-
+	it('normal success using "done"', (done) => {
+		Promise.resolve(1)
+			.then(x => {
+				expect(true).toBe(false);
+			})
+			.then(done);
 	});
 
-});
-
-describe('fromPromise$', () => {
-	it('succeeds for passing expect', (done) => {
-
-		Observable.fromPromise(Promise.resolve(1))
-			.do(() => expect(true).toBe(true))  // passes normally
-			.subscribe(null, null, done);
-
+	it('Timeout for failing "expect" using "done"', (done) => {
+		Promise.resolve(1)
+			.then(x => {
+				expect(true).toBe(false);
+			})
+			.then(done);
 	});
 
-	it('Timeout for failing expect in ".do"', (done) => {
-
-		Observable.fromPromise(Promise.resolve(1))
-			.do(x => console.log('Promise resolved:', x))
-			.do(() => expect(true).toBe(false))  // timeout instead of fail !!!
-			.do(() => console.log('Test complete'))  // never reached
-			.subscribe(null, null, done);
-
-	});
-
-	it('Timeout for failing expect in "completed" handler', (done) => {
-
-		Observable.fromPromise(Promise.resolve(2))
-			.subscribe(
-				x => console.log('Promise resolved:', x),
-				null,
-				() => {
-					expect(true).toBe(false);  // timeout instead of fail !!!
-					console.log('Test complete');  // never reached
-					done();
-			});
-
-	});
+	it('fails as expected when returning promise directly', () =>
+		Promise.resolve(1)
+			.then(x => {
+				expect(true).toBe(false);
+			})
+	);
 
 });
 
