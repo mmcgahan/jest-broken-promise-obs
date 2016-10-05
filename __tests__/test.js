35da76ecc40862f073c5daebc3a2b970dfb36678
Michael McGahan <mike.mcgahan@gmail.com>
2016-10-06 07:34:02 +1300

    additional test inside completed handler

--- __tests__/test.js
+++ __tests__/test.js
@@ -26,11 +26,28 @@ describe('fromPromise$', () => {
 			.subscribe(null, null, done);
 
 	});
-	it('Timeout for failing expect', (done) => {
+
+	it('Timeout for failing expect in ".do"', (done) => {
 
 		Observable.fromPromise(Promise.resolve(1))
+			.do(x => console.log('Promise resolved:', x))
 			.do(() => expect(true).toBe(false))  // timeout instead of fail !!!
+			.do(() => console.log('Test complete'))  // never reached
 			.subscribe(null, null, done);
+
+	});
+
+	it('Timeout for failing expect in "completed" handler', (done) => {
+
+		Observable.fromPromise(Promise.resolve(2))
+			.subscribe(
+				x => console.log('Promise resolved:', x),
+				null,
+				() => {
+					expect(true).toBe(false);  // timeout instead of fail !!!
+					console.log('Test complete');  // never reached
+					done();
+			});
 
 	});
 
